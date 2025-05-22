b2f6483f1809db6f5f2964bf8c824e6f9c77a29b
Ohazulike Stanley <sohazulike@gmail.com>
2025-05-23 09:17:10 +1000

    Add 1-multi_languages.js and updated README.md

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const cMessage = "C is fun";
+const pythonMeassage = "Python is cool";
+const jsMessage = "JavaScript is amazing";
+
+console.log(cMessage);
+console.log(pythonMeassage);
+console.log(jsMessage);
