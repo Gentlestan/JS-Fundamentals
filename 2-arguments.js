908cec9e46f64aabbfae93313bd195b76e7284b6
Ohazulike Stanley <sohazulike@gmail.com>
2025-05-23 19:29:13 +1000

    updated 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,9 @@
-const args = process.argv.length;
+const arg1 = process.argv[2];
+const arg2 = process.argv[3]
 
-if(args <= 2 ){
+if(arg1 === undefined ){
     console.log("No argument")
-}else if(args === 3 ){
+}else if(arg2 === undefined ){
     console.log("Argument found")
 }else{
     console.log("Arguments found")
